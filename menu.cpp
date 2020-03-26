1db495f4f7fd77f8ea6fe1a31eb10e5b5c291cde
Musa Gillani <62158726+MusaGillani@users.noreply.github.com>
2020-03-26 17:02:37 +0500

    Add files via upload

---
+++ menu.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+
+int main() {
+	int option = 0;
+
+
+	do {
+
+		cout << "Main Menu:" << endl << endl << endl;
+
+		cout << "	select login:" << endl << endl;
+
+		cout << "	1)Election Commission login" << endl << endl;
+
+		cout << "	2)Political Party login" << endl << endl;
+
+		cout << "	3)Voter login " << endl << endl;
+
+		cout << "	4)Exit" << endl << endl;
+
+
+		cout << "option: "; cin >> option; 
+
+		system("CLS");
+
+		switch (option)	{
+		case 1: // Election Commission
+			break;
+
+		case 2:// Political Party
+			break;
+
+		case 3:// Voter login 
+			break;
+
+		case 4:// Exit program
+			break;
+
+		default:// Invalid option
+			cout << "INVALID OPTION!!" << endl;
+			break;
+		}
+	} while (option != 4);
+
+	return 0;
+}
+
